168b5964a6ad6f36f161c97a3b35629c4bb6d989
altamash <altamashsayed47@gmail.com>
2021-07-13 01:18:25 +0530

    fixed a minor flex issue

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,48 +6,50 @@ const Contact = () => {
         <section className="contact">
             <div className="contact-container">
 
+                <div className="">
 
-                <h1>Get in touch with me</h1>
-                <div className="form-container">
-                    <form action="" className="contact-form">
+                    <h1>Get in touch with me</h1>
+                    <div className="form-container">
+                        <form action="" className="contact-form">
 
-                        <input id="name" type="text" placeholder="Enter your Name" />
+                            <input id="name" type="text" placeholder="Enter your Name" />
 
-                        <input id="email" type="email" placeholder="Enter your Email here" />
+                            <input id="email" type="email" placeholder="Enter your Email here" />
 
-                        <textarea
-                            id="message"
-                            name="message"
-                            rows="5"
-                            placeholder="Message"
-                            className="form-control"
-                        ></textarea>
-                        <button type="submit">Submit</button>
-                    </form>
+                            <textarea
+                                id="message"
+                                name="message"
+                                rows="5"
+                                placeholder="Message"
+                                className="form-control"
+                            ></textarea>
+                            <button type="submit">Submit</button>
+                        </form>
 
-                </div>
-                <h2>I am also active on</h2>
-                <div className="footer-socials">
+                    </div>
+                    <h2>I am also active on</h2>
+                    <div className="footer-socials">
 
-                    <a href="https://www.facebook.com/profile.php?id=100010723273810">
-                        <i class="fab fa-facebook-f footer-icons"></i>
-                    </a>
+                        <a href="https://www.facebook.com/profile.php?id=100010723273810">
+                            <i class="fab fa-facebook-f footer-icons"></i>
+                        </a>
 
-                    <a href="https://www.instagram.com/alps_sayed/">
-                        <i class="fab fa-instagram footer-icons"></i>
-                    </a>
+                        <a href="https://www.instagram.com/alps_sayed/">
+                            <i class="fab fa-instagram footer-icons"></i>
+                        </a>
 
-                    <a href="https://twitter.com/Altamash_sayed_">
-                        <i class="fab fa-twitter footer-icons"></i>
-                    </a>
+                        <a href="https://twitter.com/Altamash_sayed_">
+                            <i class="fab fa-twitter footer-icons"></i>
+                        </a>
 
-                    <a href="https://www.linkedin.com/in/altamash-sayed-3954b1194/">
-                        <i class="fab fa-linkedin-in footer-icons"></i>
-                    </a>
+                        <a href="https://www.linkedin.com/in/altamash-sayed-3954b1194/">
+                            <i class="fab fa-linkedin-in footer-icons"></i>
+                        </a>
 
-                    <a href="https://github.com/alta-max">
-                        <i class="fab fa-github footer-icons"></i>
-                    </a>
+                        <a href="https://github.com/alta-max">
+                            <i class="fab fa-github footer-icons"></i>
+                        </a>
+                    </div>
                 </div>
 
 
